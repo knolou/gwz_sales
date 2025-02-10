4f6b8631f00be8660aa30d227e0290c96dea9ac8
loutrekne <loutrekne@gmail.com>
2025-02-10 14:20:08 +0100

    rounding

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,5 +1,7 @@
 SELECT 
     products_id
-    ,SUM(quantity*purchase_cost) AS purchase_cost
+    ,DATE(date_date) AS purchase_date
+    ,ROUND(SUM(turnover),2) AS daily_turnover
+    ,ROUND(SUM(quantity*purchase_cost),2) AS purchase_cost
 FROM gwz_sales
 GROUP BY products_id
